cf90810a3eb0b1bfdb462e21b69bf29499661cf8
caterina vernieri <cvernier@cmslpc25.fnal.gov>
2015-02-23 17:42:34 -0600

    LMR selection

---
+++ process.sh
@@ -0,0 +1,5 @@
+root -l -b -q 'Hbb_LMR.cc+("/uscms_data/d3/cvernier/HH4b/CMSSW_7_3_1_patch1/src/VHBB_HEPPY_V8/", "glugluToX300ToHHTobbbb_13TeV_width1MeV_GEN_SIM_RECO_HLT")'
+root -l -b -q 'Hbb_LMR.cc+("/uscms_data/d3/cvernier/HH4b/CMSSW_7_3_1_patch1/src/VHBB_HEPPY_V8/", "glugluToX800ToHHTobbbb_13TeV_width1MeV_GEN_SIM_RECO_HLT")'
+#root -l -b -q 'Hbb_LMR.cc+("/uscms_data/d3/cvernier/HH4b/CMSSW_7_3_1_patch1/src/VHBB_HEPPY_V8/", "VHBB_HEPPY_V8_glugluToX1000ToHHTobbbb_13TeV_width1MeV_GEN_SIM_RECO_HLT")'
+#root -l -b -q 'Hbb_LMR.cc+("/uscms_data/d3/cvernier/HH4b/CMSSW_7_3_1_patch1/src/VHBB_HEPPY_V8/", "VHBB_HEPPY_V8_glugluToX1500ToHHTobbbb_13TeV_width1MeV_GEN_SIM_RECO_HLT")'
+root -l -b -q 'Hbb_LMR.cc+("/uscms_data/d3/cvernier/HH4b/CMSSW_7_3_1_patch1/src/VHBB_HEPPY_V8/", "glugluToX500ToHHTobbbb_13TeV_width1MeV_GEN_SIM_RECO_HLT")'
